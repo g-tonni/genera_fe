2c2ecf65ac462c0f328f5e912de67268dad99713
Giada <tonni.giada.210802@gmail.com>
2026-03-10 19:21:10 +0100

    complete: mobile menu

---
+++ src/components/MobileMenu.jsx
@@ -0,0 +1,73 @@
+import { FaDiscord } from 'react-icons/fa6'
+import { FaFacebookSquare } from 'react-icons/fa'
+import { FaInstagramSquare } from 'react-icons/fa'
+
+function MobileMenu({ panel, light }) {
+  return (
+    <div
+      className={`w-full bottom-0 pb-30 fixed z-10 bg-black transition-all duration-300 flex flex-col items-center justify-end md:justify-center
+                   ${panel === 'menu' ? 'translate-y-0 opacity-100' : 'translate-y-full opacity-0'}`}
+    >
+      <div className="flex flex-col items-center justify-center py-30">
+        <p
+          className={`${light === 'Home' ? 'text-gray-50' : 'text-gray-50/60'} text-2xl pb-8 hover:text-gray-50 transition-colors duration-150 cursor-pointer`}
+        >
+          Home
+        </p>
+        <p
+          className={`${light === 'Discover' ? 'text-gray-50' : 'text-gray-50/60'} text-2xl pb-8 hover:text-gray-50 transition-colors duration-150 cursor-pointer`}
+        >
+          Discover
+        </p>
+        <p
+          className={`${light === 'About' ? 'text-gray-50' : 'text-gray-50/60'} text-2xl hover:text-gray-50 transition-colors duration-150 cursor-pointer`}
+        >
+          About
+        </p>
+      </div>
+
+      <hr className="border border-gray-50/10 w-full" />
+
+      <div className=" w-full p-8 sm:px-15 md:px-20 flex flex-col">
+        <p className="text-gray-50/40 text-lg pb-4 hover:text-gray-50 transition-colors duration-150 cursor-pointer">
+          Terms
+        </p>
+        <p className="text-gray-50/40 text-lg pb-4 hover:text-gray-50 transition-colors duration-150 cursor-pointer">
+          Privacy Policy
+        </p>
+        <p className="text-gray-50/40 text-lg hover:text-gray-50 transition-colors duration-150 cursor-pointer">
+          Cookies
+        </p>
+      </div>
+
+      <hr className="border border-gray-50/10 w-full" />
+
+      <div className=" w-full p-8 sm:px-15 md:px-20 flex flex-col">
+        <p className="text-gray-50/40 text-lg pb-4 hover:text-gray-50 transition-colors duration-150 cursor-pointer">
+          Feedback
+        </p>
+        <p className="text-gray-50/40 text-lg hover:text-gray-50 transition-colors duration-150 cursor-pointer">
+          Contact
+        </p>
+      </div>
+
+      <hr className="border border-gray-50/10 w-full" />
+
+      <div className="w-full p-8 sm:px-15 md:px-20 flex text-gray-50/40 justify-between">
+        <div className="w-1/2 flex items-center">
+          <FaDiscord className="w-6 h-6 hover:text-gray-50 transition-colors duration-150 cursor-pointer" />
+
+          <FaFacebookSquare className="w-7 h-7 ps-3 hover:text-gray-50 transition-colors duration-150 cursor-pointer" />
+
+          <FaInstagramSquare className="w-7 h-7 ps-3 hover:text-gray-50 transition-colors duration-150 cursor-pointer" />
+        </div>
+
+        <div className="w-1/2 flex justify-end">
+          <p className="text-gray-50/40 text-lg">Genera@2026</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MobileMenu
--- src/components/NavbarMobile.jsx
+++ src/components/NavbarMobile.jsx
@@ -3,16 +3,14 @@ import { IoSearch } from 'react-icons/io5'
 import { IoMenuSharp } from 'react-icons/io5'
 import { useState } from 'react'
 
+import MobileMenu from './MobileMenu'
+
 function NavbarMobile({ light }) {
   const [panel, setPanel] = useState('')
 
   return (
     <>
-      <div
-        className={`w-full h-full z-10 bg-black ${panel === 'menu' ? ' flex' : ' hidden'}`}
-      >
-        <p className="text-white">Ciao</p>
-      </div>
+      <MobileMenu panel={panel} light={light} />
 
       <div className="fixed bottom-0 z-10 w-full h-20 mx-auto px-12 md:px-20 xl:px-25 py-6 bg-black flex lg:hidden justify-between items-center ">
         <div className="h-full w-6 cursor-pointer flex items-center">
@@ -22,9 +20,13 @@ function NavbarMobile({ light }) {
           />
         </div>
         <div
-          className="h-full w-5 text-gray-50/60  hover:text-gray-50 transition-colors duration-150 cursor-pointer"
+          className={`h-full w-5 ${panel === 'menu' ? 'text-gray-50' : 'text-gray-50/60'}  hover:text-gray-50 transition-colors duration-150 cursor-pointer`}
           onClick={() => {
-            setPanel('menu')
+            if (panel === 'menu') {
+              setPanel('')
+            } else {
+              setPanel('menu')
+            }
           }}
         >
           <IoMenuSharp className="h-full w-8" />
