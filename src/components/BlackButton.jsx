73f358e8b2cf509e1f9e803960cb46837eb751ed
Giada <tonni.giada.210802@gmail.com>
2026-03-10 11:07:01 +0100

    adds hover button

--- src/components/BlackButton.jsx
+++ src/components/BlackButton.jsx
@@ -1,6 +1,6 @@
 function BlackButton({ text }) {
   return (
-    <button className="bg-black text-gray-50 font-semibold text-xl py-4 px-10 shadow-2xl">
+    <button className="bg-black border-3 border-black text-gray-50 font-semibold text-xl py-4 px-10 shadow-2xl hover:bg-white hover:text-black transition-colors duration-150 cursor-pointer">
       {text}
     </button>
   )
--- src/components/OutlineButton.jsx
+++ src/components/OutlineButton.jsx
@@ -1,6 +1,6 @@
 function OutlineButton({ text }) {
   return (
-    <button className="font-semibold text-xl border-3 py-4 px-10">
+    <button className="font-semibold text-gray-50 text-xl border-3 border-gray-50 py-4 px-10 hover:bg-white hover:text-black transition-colors duration-150 cursor-pointer">
       {text}
     </button>
   )
