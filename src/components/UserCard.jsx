f354633d434acce27112666607fb4d2d312cd83b
Giada <tonni.giada.210802@gmail.com>
2026-03-20 13:35:10 +0100

    adds realistic counter on user card

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,7 +1,69 @@
+import { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
 function UserCard({ user }) {
   const navigate = useNavigate()
+
+  const token = useSelector((currState) => {
+    return currState.authReducer.token
+  })
+
+  const [supporters, setSupporters] = useState(null)
+  const [projects, setProjects] = useState(null)
+
+  const baseUrl = `http://localhost:3001/users/${user.userId}`
+
+  const getUserSupporters = function () {
+    fetch(baseUrl + '/supporters', {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json()
+        } else {
+          throw new Error('Errore nella response')
+        }
+      })
+      .then((data) => {
+        console.log('SUPPORTERS', data)
+        setSupporters(data.content.length)
+      })
+      .catch((err) => {
+        console.log('ERRORE: ', err)
+      })
+  }
+
+  const getUserProjects = function () {
+    fetch(baseUrl + '/projects', {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json()
+        } else {
+          throw new Error('Errore nella response')
+        }
+      })
+      .then((data) => {
+        console.log(data)
+        setProjects(data.content.length)
+      })
+      .catch((err) => {
+        console.log('ERRORE: ', err)
+      })
+  }
+
+  useEffect(() => {
+    getUserProjects()
+    getUserSupporters()
+  })
 
   return (
     <div
@@ -20,8 +82,8 @@ function UserCard({ user }) {
       <div className="flex flex-col ps-6">
         <p className="text-lg font-semibold">{user.username}</p>
         <p className="font-thin text-sm whitespace-nowrap">
-          <span className="font-medium">27</span> projects,{' '}
-          <span className="font-medium">30</span> supporters
+          <span className="font-medium">{projects}</span> projects,{' '}
+          <span className="font-medium">{supporters}</span> supporters
         </p>
       </div>
     </div>
